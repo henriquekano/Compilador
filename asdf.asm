a396ca96914d94678bb8250f3925d6ee51484736
henriquekano <henriquekano@hotmail.com>
2015-11-28 19:08:59 -0300

    RA ongoing~

--- asdf.asm
+++ asdf.asm
@@ -9,7 +9,7 @@ PARAM_1     K     /0300       ; parametro 1 da funcao
 PARAM_2     K     /0100       ; parametro 2 da funcao
 
 BP          K     /0400       ; base pointer
-FP          K     /0000       ; frame pointer
+FP          K     /0400       ; frame pointer
 SP          K     /0400       ; stack pointer
 
 RV_SIZE     K     =0008       ; tamanho do valor retornado da função
@@ -25,7 +25,13 @@ FUNCTION    K     /0000
             LD    F_PARAM_1   ; carrega o primeiro param
             -     F_PARAM_2   ; subtrai, resultado no acumulador
 
-            MM    POR_PARAM   ; guardo o valor de retorno para o pop do RA
+
+
+
+            LD    FUNCTION    ; endereco de retorno
+            MM    PUR_PARAM   
+            SC    PUSH_RA     ; push PARAM_2 na pilha de ra
+
 
             RS    FUNCTION    ; retornando
 ; fim FUNCTION -----------------------------------------------
@@ -46,26 +52,8 @@ _NEW_INSTR1 K     /0000       ; carrega na pilha de fato
             RS    PUSH_RA
 ; fim PUSH_RA --------------------------------------------
 
-; subrotina POP_RA ---------------------------------------
-; corpo da subrotina
-POP_RA      K     /0000
-            LD    MM_INSTR    ; composicao do comando de carregar na pilha
-            +     SP
-            -     TWO
-            MM    _NEW_INSTR1
-            LD    PUR_PARAM
-_NEW_INSTR1 K     /0000       ; carrega na pilha de fato  
-            LD    SP          
-            -     TWO
-            MM    SP          ; sp, aponte para o vazio 
-            RS    POP_RA
-; fim POP_RA --------------------------------------------
-
 ; corpo do main -----------------------------------------
-MAIN        LD    SP
-            MM    FP_AUX      ; carrega a possivel nova FP 
-
-            LD    PARAM_1     
+MAIN        LD    PARAM_1     
             MM    PUR_PARAM   
             SC    PUSH_RA     ; push PARAM_1 na pilha de ra
 
@@ -76,9 +64,6 @@ MAIN        LD    SP
             LD    FP     
             MM    PUR_PARAM         
             SC    PUSH_RA     ; push PARAM_2 na pilha de ra
-
-            LD    FP_AUX
-            MM    FP          ; atualizo a FP
 
             SC    FUNCTION    ; chama FUNCTION
 
--- mvn_mont.sh
+++ mvn_mont.sh
@@ -1,3 +1,3 @@
 java -cp MLR.jar montador.MvnAsm asdf.asm
 java -cp MLR.jar relocator.MvnRelocator asdf.mvn 0000 000
-java -Dfile.encoding=cp850 -jar MVN2013_v2.jar
+java -Dfile.encoding=cp850 -jar mvn.jar
