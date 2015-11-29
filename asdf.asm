8abb9709eb345994d83db193ca247b60ab6de53f
mickawaii <mickawaii@gmail.com>
2015-11-28 23:25:10 -0200

    finished RA

--- asdf.asm
+++ asdf.asm
@@ -8,43 +8,71 @@ MM_INSTR    MM    /0000       ; para fabricar instrucao de MM
 PARAM_1     K     /0300       ; parametro 1 da funcao
 PARAM_2     K     /0100       ; parametro 2 da funcao
 
-BP          K     /0400       ; base pointer
 FP          K     /0400       ; frame pointer
 SP          K     /0400       ; stack pointer
 
-RV_SIZE     K     =0008       ; tamanho do valor retornado da função
-
-; subrotina FUNCTION ---------------------------------------
+; subrotina SUB ---------------------------------------
 F_PARAM_1   K     /0000
 F_PARAM_2   K     /0000
+F_RESULT    K     /0000
 ; corpo da subrotina
-FUNCTION    K     /0000
+SUB         K     /0000
+            LD    SUB    ; endereco de retorno
+            MM    PUSH_P   
+            SC    PUSH_RA     ; push ADDR na pilha de ra
             
+            LD    SP
+            MM    FP          ; fp aponta para o ADDR na pilha de ra
+
+            LV    =6
+            MM    GET_P       
+            SC    GET_RA      ; carrega param 1 partir de endereco relativo no ra
+            MM    F_PARAM_1
             
+            LV    =4
+            MM    GET_P       
+            SC    GET_RA      ; carrega param 2 a partir de endereco relativo no ra
+            MM    F_PARAM_2
 
             LD    F_PARAM_1   ; carrega o primeiro param
-            -     F_PARAM_2   ; subtrai, resultado no acumulador
+            -     F_PARAM_2   ; 
+            MM    RTRN_RSLT   ; subtrai e guarda para return
 
+            JP    RETURN
 
+; fim SUB -----------------------------------------------
 
+; RETURN ---------------------------------------
+RTRN_RSLT   K     /0000
+RTRN_ADDR   K     /0000
+; corpo da subrotina
+RETURN      LD    RTRN_RSLT
+            MM    PUSH_P   
+            SC    PUSH_RA     ; push RESULT na pilha de ra
 
-            LD    FUNCTION    ; endereco de retorno
-            MM    PUR_PARAM   
-            SC    PUSH_RA     ; push PARAM_2 na pilha de ra
+            LV    =0
+            MM    GET_P
+            SC    GET_RA
+            MM    RTRN_ADDR   ; endereco de retorno recuperado da pilha
 
+            LD    TWO
+            MM    GET_P
+            SC    GET_RA
+            MM    SP          ; dropa a ra atual
+            MM    FP          ; volta a fp antiga
 
-            RS    FUNCTION    ; retornando
-; fim FUNCTION -----------------------------------------------
+            RS    RTRN_ADDR   ; retorna para o escopo anterior
+; fim RETURN --------------------------------------------
 
 ; subrotina PUSH_RA ---------------------------------------
-PUR_PARAM    K     /0000
+PUSH_P      K     /0000
 ; corpo da subrotina
 PUSH_RA     K     /0000
             LD    MM_INSTR    ; composicao do comando de carregar na pilha
             +     SP
             -     TWO
             MM    _NEW_INSTR1
-            LD    PUR_PARAM
+            LD    PUSH_P
 _NEW_INSTR1 K     /0000       ; carrega na pilha de fato  
             LD    SP          
             -     TWO
@@ -52,20 +80,32 @@ _NEW_INSTR1 K     /0000       ; carrega na pilha de fato
             RS    PUSH_RA
 ; fim PUSH_RA --------------------------------------------
 
+; subrotina GET_RA ---------------------------------------
+GET_P       K     /0000       ; endereco relativo na ra
+; corpo da subrotina
+GET_RA      K     /0000
+            LD    LD_INSTR
+            +     FP          ; 
+            +     GET_P       ;
+            MM    _NEW_INSTR2 ;
+_NEW_INSTR2 K     /0000       ; carrega valor da ra no acumulador
+            RS    GET_RA
+; fim GET_RA --------------------------------------------
+
 ; corpo do main -----------------------------------------
 MAIN        LD    PARAM_1     
-            MM    PUR_PARAM   
+            MM    PUSH_P   
             SC    PUSH_RA     ; push PARAM_1 na pilha de ra
 
             LD    PARAM_2     
-            MM    PUR_PARAM   
+            MM    PUSH_P   
             SC    PUSH_RA     ; push PARAM_2 na pilha de ra
 
             LD    FP     
-            MM    PUR_PARAM         
-            SC    PUSH_RA     ; push PARAM_2 na pilha de ra
+            MM    PUSH_P         
+            SC    PUSH_RA     ; push old FP na pilha de ra
 
-            SC    FUNCTION    ; chama FUNCTION
+            SC    SUB    ; chama SUB
 
             HM    INIT        ; fim do programa inteiro
 ;fim main -----------------------------------------------
